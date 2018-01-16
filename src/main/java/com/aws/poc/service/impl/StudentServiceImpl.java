69c77628b42e0613e2b7756a47a7f701f6352906
sxb9638 <sudipto.bhowmick@yum.com>
2018-01-16 07:00:24 -0500

    Added java 8 features

--- src/main/java/com/aws/poc/service/impl/StudentServiceImpl.java
+++ src/main/java/com/aws/poc/service/impl/StudentServiceImpl.java
@@ -1,31 +1,54 @@
 package com.aws.poc.service.impl;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Stream;
+
+import javax.annotation.PostConstruct;
 
 import org.springframework.stereotype.Service;
 
 import com.aws.poc.dto.Student;
-import com.aws.poc.init.SpringBootInitializer;
 import com.aws.poc.service.StudentService;
 
 @Service
 public class StudentServiceImpl implements StudentService {
 
+	
+	public List<Student> students = new ArrayList<Student>();
+	
+	public Comparator<Student> sortByRollNumber = (student1, student2) -> student1.getRollNumber().compareTo(student2.getRollNumber());
+	
+	@PostConstruct
+	public void buildStudentCache(){
+		Student s1 = new Student("Sudipto", 12345, "Science", "ST THOMAS HIGH SCOOL");
+		Student s2 = new Student("Samba", 32347, "Science", "DON BOSCO");
+		Student s3 = new Student("Sourav", 89875, "Commerce", "SOUTH POINT");
+		
+		students.add(s1);
+		students.add(s2);
+		students.add(s3);
+		Collections.sort(students, sortByRollNumber);
+	}
+	
 	@Override
 	public List<Student> getStudents() {
-		return SpringBootInitializer.students;
+		return students;
 	}
 
 	@Override
 	public List<Student> addStudent(Student newStudent) {
-		SpringBootInitializer.students.add(newStudent);
-		return SpringBootInitializer.students;
+		students.add(newStudent);
+		Collections.sort(students, sortByRollNumber);
+		return students;
 	}
 
 	@Override
 	public List<Student> updateStudents(Student modifyStudent) {
 		Student record = null;
-		for (Student data: SpringBootInitializer.students) {
+		for (Student data: students) {
 			if(data != null && data.getRollNumber().equals(modifyStudent.getRollNumber())){
 				record = data;
 			}
@@ -36,23 +59,25 @@ public class StudentServiceImpl implements StudentService {
 			record.setName(modifyStudent.getName());
 			record.setSchoolName(modifyStudent.getSchoolName());
 		}
-		return SpringBootInitializer.students;
+		
+		Collections.sort(students, sortByRollNumber);
+		return students;
 	}
 
 	@Override
 	public List<Student> deleteStudent(Integer rollNumber) {
-		// TODO Auto-generated method stub
-		Student record = null;
-		for (Student data: SpringBootInitializer.students) {
-			if(data != null && data.getRollNumber().equals(rollNumber)){
-				record = data;
-			}
-		}
+		List<Student> record = new ArrayList<Student>();
+		Stream<Student> studentStream = students .stream();
+		//Predicate<Student> selectData = (Student record1) -> record1.getRollNumber().equals(1);
+		studentStream.filter(student -> student.getRollNumber().equals(rollNumber))
+					 .forEach(record::add);;
 		
 		if(record != null){
-			SpringBootInitializer.students.remove(record);
+			students.removeAll(record);
 		}
-		return SpringBootInitializer.students;
+		
+		Collections.sort(students, sortByRollNumber);
+		return students;
 	}
 
 }
